30ecb8d33ad1583943f754d861eb5eb15cd3ea3d
2005Interceptor <30561683+2005Interceptor@users.noreply.github.com>
2017-08-15 21:30:15 -0700

    BubbleSort
    Do not know why the first function does not work.

---
+++ Bubblesort.py
@@ -0,0 +1,23 @@
+"""              
+def bubbleSort(a):
+    for i in range (0, len(a)-1, 1): 
+        for j in range(len(a), i+1, -1):
+            if a[j] < a[j-1]:
+                temp = a[j]
+                a[j] = a[j-1]
+                a[j-1] = temp 
+    return a  
+"""
+
+def bubbleSort(a):
+    for passnum in range(len(a)-1,0,-1):
+        for i in range(passnum):
+            if a[i] > a[i+1]:
+                temp = a[i]
+                a[i] = a[i+1]
+                a[i+1] = temp 
+    return alist
+
+alist = [0, 1, 6, 2, 4, 14, 9, 13]
+alist2 = bubbleSort(alist)
+print(alist2)
